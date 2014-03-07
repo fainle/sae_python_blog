7a6cc14fc0c8229f6446a828a48e1aac136c1fa8
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2014-03-07 09:29:00 +0000

    git-svn-id: https://svn.sinaapp.com/dilan/1@1122 288da536-3c93-4ab2-bc2e-17a84f795b60

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -5,4 +5,16 @@ main_page = Blueprint('main_page', __name__)
 
 @main_page.route("/")
 def main():
+    """
+    main page
+    """
     return render_template('index.html')
+
+
+@main_page.route("/test")
+@main_page.route("/test.html")
+def test():
+    """
+    test
+    """
+    return render_template('test.html')
