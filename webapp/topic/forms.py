eae4623cfc521113284b87de2bfe669155515643
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2014-03-12 09:18:45 +0000

    add
    git-svn-id: https://svn.sinaapp.com/dilan/1@1137 288da536-3c93-4ab2-bc2e-17a84f795b60

---
+++ webapp/topic/forms.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from package.flask_wtf.form import Form
+from package.wtforms import TextField, TextAreaField, validators, HiddenField, IntegerField, SelectField
+from package.wtforms.validators import Required, Length, ValidationError, Email
+from domain.model.topic import Category
+
+
+class CategoryForm(Form):
+    """
+    category form
+    """
+    id = HiddenField('id', default=0)
+    name = TextField('name', validators=[
+        Required(u'分类名称不能为空'),
+        Length(min=2, max=20, message=u'分类名称不能太长')
+    ])
+
+
+class TopicForm(Form):
+    """
+    topic form
+    """
+    title = TextField('title', validators=[
+        Required(u'标题不能为空')
+    ])
+    content = TextAreaField('content', validators=[
+        Required(u'内容不能为空')
+    ])
+    priority = IntegerField('priority', default=0)
+    category_id = SelectField('category_id', choices=[], coerce=int)
+    tag = TextField('tag', default='')
--- webapp/topic/views.py
+++ webapp/topic/views.py
@@ -1,7 +1,8 @@
 __author__ = 'apple'
-from flask import Blueprint, render_template, g, abort
+from flask import Blueprint, render_template, g, abort, request, redirect
 from domain import db_session
-from domain.model.topic import Topic
+from domain.model.topic import Topic, Category, TopicTag, TopicToTag
+from webapp.topic.forms import TopicForm
 
 
 topic_page = Blueprint('topic_page', __name__)
@@ -28,3 +29,69 @@ def topic_show(id=0):
                            #count=topic.count,
                            #reply_form=reply_form,
                            #reply=reply,)
+
+
+@topic_page.route('/topic/add', methods=("POST", "GET"))
+@topic_page.route('/topic/add/<int:id>', methods=("POST", "GET"))
+#@login_required
+#@is_admin
+def topic_add(id=0):
+    topic = Topic.query.filter(Topic.id == id).first()
+    topic_form = TopicForm(obj=topic)
+    topic_form.category_id.choices = [(c.id, c.name) for c in Category.query.order_by(Category.id.asc()).all()]
+    if request.method == 'POST' and topic_form.validate():
+        if topic:
+            if topic.category_id != topic_form.category_id.data:
+                new_category = Category.get(topic_form.category_id.data)
+                new_category.num += 1
+                old_category = Category.get(topic.category_id)
+                old_category.num -= 1
+
+            tag_list = topic_form.tag.data.split(" ")
+            for tag_name in tag_list:
+                if tag_name.strip():
+                    tag = TopicTag.query.filter(TopicTag.name == tag_name).first()
+                    if tag:
+                        topic_to_tag = TopicToTag.query.filter(TopicToTag.topic_id == id).filter(
+                            TopicToTag.tag_id == tag.id).first()
+                        if topic_to_tag is None:
+                            tag.num += 1
+                            topic_to_tag = TopicToTag(id, tag.id)
+                            db_session.add(topic_to_tag)
+                    else:
+                        tag = TopicTag(tag_name)
+                        db_session.add(tag)
+                        db_session.flush()
+                        topic_to_tag = TopicToTag(topic.id, tag.id)
+                        db_session.add(topic_to_tag)
+
+            topic_form.populate_obj(topic)
+            db_session.commit()
+        else:
+            topic = Topic(topic_form.title.data)
+            topic.content = topic_form.content.data
+            topic.category_id = topic_form.category_id.data
+            topic.tag = topic_form.tag.data
+            db_session.add(topic)
+            db_session.flush()
+
+            category = Category.get(topic_form.category_id.data)
+            category.num += 1
+            tag_list = topic_form.tag.data.split(" ")
+            for tag_name in tag_list:
+                if tag_name.strip():
+                    tag = TopicTag.query.filter(TopicTag.name == tag_name).first()
+                    if tag:
+                        tag.num += 1
+                    else:
+                        tag = TopicTag(tag_name)
+                        db_session.add(tag)
+                        db_session.flush()
+                    topic_to_tag = TopicToTag(topic.id, tag.id)
+                    db_session.add(topic_to_tag)
+            db_session.commit()
+        return redirect('/')
+    else:
+        return render_template('/topic/add.html',
+                               topic_form=topic_form,
+        )
