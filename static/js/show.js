1738059b21d0c58982b34a1428519d4e1fee8be6
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2014-04-02 06:49:51 +0000

    u
    git-svn-id: https://svn.sinaapp.com/dilan/1@1263 288da536-3c93-4ab2-bc2e-17a84f795b60

--- static/js/show.js
+++ static/js/show.js
@@ -1,2 +1,36 @@
-alert("hehe");
-alert();
+require.config({
+    baseUrl:'/static/',
+    paths:{
+        "jquery":"js/jquery",
+        "markitup":"markitup/jquery.markitup",
+        "set":"markitup/sets/markdown/set"
+    }
+})
+
+require(['jquery','markitup',"set"], function($, markItUp, mySettings){
+    $(function() {
+        $('#markItUp').markItUp(mySettings);
+        //pic uplaod
+        $("#upload_pic").click(function() {
+            $.ajaxFileUpload ({
+                url:"http://dilan.sinaapp.com/upload",
+                fileElementId:"file",
+                dataType:"json",
+                success:function(data){
+                    var url = "/file/" + data.name;
+                    $("#pic_url").val(url)
+                }
+            })
+        });
+        //insert pic url
+        $("#insert_pic").click(function() {
+            src = $("#pic_url").val();
+            alt = $("#pic_title").val();
+            $.markItUp(
+                { replaceWith:'<img src="'+src+'" alt="'+alt+'" title="'+alt+'"(!( class="[![Class]!]")!) />' }
+            );
+            $('#upload').modal('hide');
+            return false;
+        });
+    });
+});
