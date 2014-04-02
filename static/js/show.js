68746e8b0c104117440e820ab487ce9286cbc79b
287988841@qq.com <287988841@qq.com@288da536-3c93-4ab2-bc2e-17a84f795b60>
2014-04-02 06:59:20 +0000

    u
    git-svn-id: https://svn.sinaapp.com/dilan/1@1264 288da536-3c93-4ab2-bc2e-17a84f795b60

--- static/js/show.js
+++ static/js/show.js
@@ -7,9 +7,39 @@ require.config({
     }
 })
 
-require(['jquery','markitup',"set"], function($, markItUp, mySettings){
+
+mySettings = {
+	previewParserPath:	'',
+	onShiftEnter:		{keepDefault:false, openWith:'\n\n'},
+	markupSet: [
+		{name:'First Level Heading', key:'1', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '=') } },
+		{name:'Second Level Heading', key:'2', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '-') } },
+		{name:'Heading 3', key:'3', openWith:'### ', placeHolder:'Your title here...' },
+		{name:'Heading 4', key:'4', openWith:'#### ', placeHolder:'Your title here...' },
+		{name:'Heading 5', key:'5', openWith:'##### ', placeHolder:'Your title here...' },
+		{name:'Heading 6', key:'6', openWith:'###### ', placeHolder:'Your title here...' },
+		{separator:'---------------' },
+		{name:'Bold', key:'B', openWith:'**', closeWith:'**'},
+		{name:'Italic', key:'I', openWith:'_', closeWith:'_'},
+		{separator:'---------------' },
+		{name:'Bulleted List', openWith:'- ' },
+		{name:'Numeric List', openWith:function(markItUp) {
+			return markItUp.line+'. ';
+		}},
+		{separator:'---------------' },
+		{name:'Picture', key:'P', replaceWith:'![[![Alternative text]!]]([![Url:!:http://]!] "[![Title]!]")'},
+		{name:'Link', key:'L', openWith:'[', closeWith:']([![Url:!:http://]!] "[![Title]!]")', placeHolder:'Your text to link here...' },
+		{separator:'---------------'},
+		{name:'Quotes', openWith:'> '},
+		{name:'Code Block / Code', openWith:'(!(\t|!|`)!)', closeWith:'(!(`)!)'},
+		{separator:'---------------'},
+		{name:'Preview', call:'preview', className:"preview"}
+	]
+}
+
+require(['jquery','markitup',"set"], function($){
     $(function() {
-        $('#markItUp').markItUp(mySettings);
+        $('#markItUp').$.fn.markItUp(mySettings);
         //pic uplaod
         $("#upload_pic").click(function() {
             $.ajaxFileUpload ({
