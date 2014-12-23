1f4e05c8d7a3c71e74db0e0d77e41ed9c4411013
fainle <1361238180@qq.com>
2014-12-23 11:57:40 +0800

    init blueprint and admin blueprint

--- webapp/index/view.py
+++ webapp/index/view.py
@@ -5,6 +5,16 @@ from flask import Blueprint, render_template
 index_page = Blueprint('index_page', __name__)
 
 
+@index_page.before_request
+def before_request():
+	pass
+
+
 @index_page.route('/')
 def index():
 	return render_template('index/index.html')
+
+
+@index_page.teardown_request
+def teardown_request(exception):
+	pass
